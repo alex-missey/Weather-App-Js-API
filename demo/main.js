b0c363313a0291d953e8e43fda589491e554a464
Jack Kelly <JKelly423@users.noreply.github.com>
2023-07-15 18:36:15 -0500

    Refactored for live web demo from /demo

---
+++ demo/main.js
@@ -0,0 +1,114 @@
+import axios from "axios";
+import "./style.css"
+import { getWeather } from "./weather"
+import { ICON_MAP } from "./icon-map"
+
+/* Declare all constants at the begining of the file so they are accessible instantly at runtime */
+
+// QuerySelectors 
+const currentIcon = document.querySelector("[data-current-icon]")
+// Check if value null to avoid typeErrors
+currentIcon === null ? console.warn(`currentIcon ([data-current-icon]) is NULL!`) : null
+
+const dailySection = document.querySelector("[data-day-section]")
+dailySection === null ? console.warn(`dailySection ([data-day-section]) is NULL!`) : null
+const hourlySection = document.querySelector("[data-hour-section]")
+hourlySection === null ? console.warn(`hourlySection ([data-hour-section]) is NULL!`) : null
+// Templates
+const dayCardTemplate = document.getElementById("day-card-template")
+dayCardTemplate === null ? console.warn(`dayCardTemplate (day-card-template) is NULL!`) : null
+const hourRowTemplate = document.getElementById("hour-row-template")
+hourRowTemplate === null ? console.warn(`hourRowTemplate (hour-row-template) is NULL!`) : null
+// DateTimeFormats
+const TIMEZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" })
+
+/* Declare all constants at the begining of the file so they are accessible instantly at runtime */
+
+// Set to a variable and use 'await' so we don't have to use .then()
+// This will allow us to catch errors in getWeather and renderWeather individually to help narrow the scope of the problem
+// since axios.get() returns a promise, we need to use 'await' to wait for that promise to resolve before rendering the data
+// the reason this was not required before, is because .then() awaits the promise's resolution before running
+let data = await getWeather(10, 10, TIMEZONE).catch((err) => {
+  console.error(err);
+  alert("Error when setting data with getWeather");
+});
+
+// If data is not null, console.log the response from getWeather
+data ? console.log(`getWeather response\n-------------------`) : null
+data ? console.log(data) : null
+
+// We need to use a try-catch block here instead of .catch(), since .catch() can only be used on promises, which is either 'await', .then(), or try{}
+try {
+  renderWeather(data)
+} catch (err) {
+  console.error(err);
+  alert("Error when rendering weather with rednerWeather");
+}
+
+// intl.datetimeformat just gives us the time of the current timezone we are in
+// .then() returns to us a promise, res means result
+// took out res and just put data because we aren't getting a response
+// put .then(renderWeather) and deleted the rest
+// put .catch on there in case something goes wrong
+
+function renderWeather({ current, daily, hourly }) {
+  // use individual functions
+  renderCurrentWeather(current)
+  renderDailyWeather(daily)
+  renderHourlyWeather(hourly)
+  // remove blurred class
+  document.body.classList.remove("blurred")
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value
+  // started everything with data- so pass everything after that
+}
+
+function getIconUrl(iconCode) {
+  return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+
+
+function renderCurrentWeather(current) {
+  //document.querySelector("[data-current-temp]").textContent = current.currentTemp
+  // Would need to do this ^^^ multiple times, make helper function instead
+  currentIcon.src = getIconUrl(current.iconCode)
+  setValue("current-temp", current.currentTemp)
+  setValue("current-high", current.highTemp)
+  setValue("current-low", current.lowTemp)
+  setValue("current-fl-high", current.highFeelsLike)
+  setValue("current-fl-low", current.lowFeelsLike)
+  setValue("current-wind", current.windSpeed)
+  setValue("current-precip", current.precip)
+}
+
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = ""
+  daily.forEach(day => {
+    const element = dayCardTemplate.content.cloneNode(true)
+    // makes sure it clones all of its children (clone a template)
+    setValue("temp", day.maxTemp, { parent: element })
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element })
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode)
+    dailySection.append(element)
+  })
+}
+
+function renderHourlyWeather(hourly) {
+  hourlySection.innerHTML = ""
+  hourly.forEach(hour => {
+    const element = hourRowTemplate.content.cloneNode(true)
+    // makes sure it clones all of its children (clone a template)
+    setValue("temp", hour.temp, { parent: element })
+    setValue("fl-temp", hour.feelsLike, { parent: element })
+    setValue("wind", hour.windSpeed, { parent: element })
+    setValue("precip", hour.precip, { parent: element })
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element })
+    setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element })
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode)
+    hourlySection.append(element)
+  })
+}
---
+++ demo/weather.js
@@ -0,0 +1,88 @@
+/* Comment out import for web 
+import axios from "axios";
+*/
+
+// For our API
+// We want all dates in our timezone and need latitude and longitude
+// Starting with everything before our query (up to forecast)
+// After adding params delete latitude and longitude from link
+// Also remove
+
+export function getWeather(lat, lon, timezone) {
+    return axios.get(
+        "https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+        {
+            params: {
+                latitude: lat,
+                longitude: lon,
+                timezone,
+            }
+        }
+    ).then(({ data }) => {
+        //return data
+        return {
+            current: parseCurrentWeather(data),
+            daily: parseDailyWeather(data),
+            hourly: parseHourlyWeather(data),
+        } // Created 3 functions to parse out this data so we will build a function below
+    })
+}
+
+function parseCurrentWeather({ current_weather, daily }) {
+    const {
+        temperature: currentTemp,
+        windSpeed: windSpeed,
+        weathercode: iconCode
+    } = current_weather
+    const {
+        temperature_2m_max: [maxTemp], // array
+        temperature_2m_min: [minTemp],
+        apparent_temperature_max: [maxFeelsLike],
+        apparent_temperature_min: [minFeelsLike],
+        precipitation_sum: [precip],
+
+    } = daily
+
+    // **********ABOVE IS SAME AS THIS***************
+    // const maxTemp = daily.temperature_2m_max[0]
+
+
+    // Want to round off temps (no decimals)
+    return {
+        currentTemp: Math.round(currentTemp),
+        highTemp: Math.round(maxTemp),
+        lowTemp: Math.round(minTemp),
+        highFeelsLike: Math.round(maxFeelsLike),
+        lowFeelsLike: Math.round(minFeelsLike),
+        windSpeed: Math.round(windSpeed),
+        precip: Math.round(precip * 100) / 100, // Rounds to nearest hundredth
+        iconCode,
+    } // These are the different things we need to get from the API ^^^
+}
+
+function parseDailyWeather({ daily }) {
+    return daily.time.map((time, index) => {
+        // Loop through object, get time value (array) and map over that array (time value)
+        return { // returns object containing data we need (date, temp, and icon)
+            timestamp: time * 1000, // need timestamp in ms for JS
+            iconCode: daily.weathercode[index], // want value at current index (day 1 etc)
+            maxTemp: Math.round(daily.temperature_2m_max[index])
+        }
+    })
+}
+
+function parseHourlyWeather({ hourly, current_weather }) {
+    // need current weather to know what hour to start at
+    return hourly.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: hourly.weathercode[index],
+            temp: Math.round(hourly.temperature_2m[index]),
+            feelsLike: Math.round(hourly.apparent_temperature[index]),
+            windSpeed: Math.round(hourly.windspeed_10m[index]),
+            precip: Math.round(hourly.precipitation[index] * 100) / 100, // extra precision on rounding
+            // haven't filtered what our current time is using current_weather yet
+
+        }
+    }).filter(({ timestamp }) => timestamp >= current_weather.time * 1000)
+}
