f170d40defd1647eec01ca21359d876c124d005b
Jack Kelly <58000611+JKelly423@users.noreply.github.com>
2023-07-15 19:05:47 -0500

    Update weather-web.js

--- docs/weather-web.js
+++ docs/weather-web.js
@@ -5,7 +5,25 @@
 // After adding params delete latitude and longitude from link
 // Also remove
 
-export function getWeather(lat, lon, timezone) {
+const ICON_MAP = new Map()
+
+addMapping([0, 1], "sun")
+addMapping([2], "cloud-sun")
+addMapping([3], "cloud")
+addMapping([45, 48], "smog")
+addMapping([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82], "cloud-showers-heavy")
+addMapping([71, 73, 75, 77, 85, 86], "snowflake")
+addMapping([95, 96, 99], "cloud-bolt")
+
+
+
+function addMapping(values, icon) {
+    values.forEach(value => {
+        ICON_MAP.set(value, icon)
+    })
+}
+
+function getWeather(lat, lon, timezone) {
     return get(
         "https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
         {
